348ee99746f24f549b850ce9601aa5eb11e72681
Aka1t0 <e79123834623@gmail.com>
2023-09-13 19:44:17 +0500

    first commit

--- Blog/blogApp/models.py
+++ Blog/blogApp/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.urls import reverse
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    email = models.EmailField(blank=True)
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    body = models.TextField()
+    title = models.CharField(max_length=200, verbose_name='Именование')
+    email = models.EmailField(blank=True, verbose_name='Голубиная почта')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name='Творец')
+    body = models.TextField(verbose_name='Наполнение')
 
     def __str__(self):
         return self.title
